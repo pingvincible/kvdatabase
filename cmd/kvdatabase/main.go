579dfd6847628a11f1419b1787215ed6a2c3b79b
Sergey Possokhov <pingvincible@gmail.com>
2025-01-15 12:22:29 +0500

    Add command parser

--- cmd/kvdatabase/main.go
+++ cmd/kvdatabase/main.go
@@ -1,16 +1,36 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
 	"log/slog"
+	"os"
 
+	"github.com/pingvincible/kvdatabase/internal/compute/parser"
 	"github.com/pingvincible/kvdatabase/internal/logger"
 )
 
 func main() {
-	logger.Configure(slog.LevelDebug)
-	slog.Info("Database started")
-	// Configure logger.
 	// Create parser.
 	// Create engine.
 	// Create interactive shell.
+	logger.Configure(slog.LevelDebug)
+	slog.Info("Database started")
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Printf("> ")
+		scanner.Scan()
+		text := scanner.Text()
+
+		command, err := parser.Parse(text)
+		if err != nil {
+			fmt.Printf("%s\n", err)
+
+			continue
+		}
+
+		command.Execute()
+	}
 }
--- internal/compute/parser/parser.go
+++ internal/compute/parser/parser.go
@@ -1,1 +1,134 @@
 package parser
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"regexp"
+	"strings"
+)
+
+type CommandType string
+
+const (
+	CommandSet CommandType = "SET"
+	CommandGet CommandType = "GET"
+	CommandDel CommandType = "DEL"
+
+	CommandSetArgsNumber = 2
+)
+
+type Command struct {
+	Type  CommandType
+	Key   string
+	Value string
+}
+
+func (c *Command) String() string {
+	return fmt.Sprintf("Type: %s, %s=%s", c.Type, c.Key, c.Value)
+}
+
+func (c *Command) Execute() {
+	slog.Info("Executing command", slog.String("command", c.String()))
+}
+
+var (
+	ErrUnknownCommand     = errors.New("unknown command")
+	ErrNotEnoughArguments = errors.New("not enough arguments")
+	ErrInvalidArgument    = errors.New("invalid argument")
+)
+
+func Parse(command string) (Command, error) {
+	if len(command) == 0 {
+		return Command{}, ErrUnknownCommand
+	}
+
+	args := strings.Split(command, ` `)
+	if len(args) == 1 {
+		return Command{}, ErrNotEnoughArguments
+	}
+
+	switch args[0] {
+	case string(CommandSet):
+		return parseCommandSet(args[1:])
+	case string(CommandGet):
+		return parseCommandGet(args[1:])
+	case string(CommandDel):
+		return parseCommandDel(args[1:])
+	default:
+		return Command{}, ErrUnknownCommand
+	}
+}
+
+func parseCommandSet(args []string) (Command, error) {
+	if len(args) < CommandSetArgsNumber {
+		return Command{}, ErrNotEnoughArguments
+	}
+
+	err := validate(args)
+	if err != nil {
+		return Command{}, err
+	}
+
+	return Command{
+		Type:  CommandSet,
+		Key:   args[0],
+		Value: args[1],
+	}, nil
+}
+
+func parseCommandGet(args []string) (Command, error) {
+	if len(args) == 0 {
+		return Command{}, ErrNotEnoughArguments
+	}
+
+	err := validate(args)
+	if err != nil {
+		return Command{}, err
+	}
+
+	return Command{
+		Type:  CommandGet,
+		Key:   args[0],
+		Value: "",
+	}, nil
+}
+
+func parseCommandDel(args []string) (Command, error) {
+	if len(args) == 0 {
+		return Command{}, ErrNotEnoughArguments
+	}
+
+	err := validate(args)
+	if err != nil {
+		return Command{}, err
+	}
+
+	return Command{
+		Type:  CommandDel,
+		Key:   args[0],
+		Value: "",
+	}, nil
+}
+
+func validate(args []string) error {
+	for _, arg := range args {
+		err := validateArgument(arg)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func validateArgument(arg string) error {
+	r := regexp.MustCompile("^[a-zA-Z0-9_/*]+$")
+
+	matched := r.MatchString(arg)
+	if !matched {
+		return ErrInvalidArgument
+	}
+
+	return nil
+}
